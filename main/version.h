76653b7592b66e1c94e6362952f9bab5cd294a22
Pankaj1215 <pankaj.sharma@audertec.com>
2020-12-05 00:03:01 +0530

    FW_1_6_9_Ver_Modified

--- main/version.h
+++ main/version.h
@@ -28,7 +28,7 @@
 
 #define FW_VERSION_MAJOR               (1)
 #define FW_VERSION_MINOR               (6)
-#define FW_VERSION_REVISION      (8)// (7)// (6) //  (5)// (4)//(3)// (2)// (0)  // old one was 0, 1 is only for testing ota
+#define FW_VERSION_REVISION     (9)// (8)// (7)// (6) //  (5)// (4)//(3)// (2)// (0)  // old one was 0, 1 is only for testing ota
 
 /*!
  * \fn uint32_t get_version_major(void)
